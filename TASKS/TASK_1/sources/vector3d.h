79234f33a572eec1b37bbb334032e13e5ffb713b
reeFridge <reefridgerator@gmail.com>
2016-09-30 04:11:07 +0700

    Fixed mistakes in methods arguments and added constructor/destructor declaration

--- TASKS/TASK_1/sources/vector3d.h
+++ TASKS/TASK_1/sources/vector3d.h
@@ -8,21 +8,23 @@ class Vector3D {
 	Cheshire* smile;
 	
 	public:
+		Vector3D();
+		~Vector3D();
 		double		getElement(int);
 		void		setElement(int, double);
 		double		getModule();
-		Vector3D	copy();
+		Vector3D*	copy();
 		void		multiplyByScalar(double);
 		void		normalize();
 		void		print();
 
-		static		Vector3D	add(&Vector3D, &Vector3D);
-		static		Vector3D	substruct(&Vector3D, &Vector3D);
-		static		Vector3D	vectorMultiply(&Vector3D, &Vector3D);
-		static		double		scalarMultiply(&Vector3D, &Vector3D);
-		static		double		sin(&Vector3D, &Vector3D);
-		static		double		cos(&Vector3D, &Vector3D);
-		static		double		angle(&Vector3D, &Vector3D);
-}
+		static		Vector3D*	add(Vector3D&, Vector3D&);
+		static		Vector3D*	substruct(Vector3D&, Vector3D&);
+		static		Vector3D*	vectorMultiply(Vector3D&, Vector3D&);
+		static		double		scalarMultiply(Vector3D&, Vector3D&);
+		static		double		sin(Vector3D&, Vector3D&);
+		static		double		cos(Vector3D&, Vector3D&);
+		static		double		angle(Vector3D&, Vector3D&);
+};
 
 #endif // VECTOR_H
