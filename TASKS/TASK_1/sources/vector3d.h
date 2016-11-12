b74ae5b33ec0f0ac4614e831043c65cdb38ee5f2
reeFridge <reefridgerator@gmail.com>
2016-11-12 13:37:44 +0700

    Change signatures of methods and fields of class

--- TASKS/TASK_1/sources/vector3d.h
+++ TASKS/TASK_1/sources/vector3d.h
@@ -8,31 +8,34 @@ namespace classes {
 		double x, y, z;
 	};
 
+	typedef const char* err;
+
 	class Vector3D {
 		struct Cheshire;
 		Cheshire* smile;
 
-		static const Point3D nullPoint = { 0.0, 0.0, 0.0 };
+		static const Point3D nullPoint;
+		static unsigned int idCounter;
 	
 		public:
-			Vector3D();
 			Vector3D(Point3D point = nullPoint);
-			Vector3D(double x = 0.0, double y = 0.0, double z = 0.0);
+			Vector3D(double x, double y = 0.0, double z = 0.0);
 			~Vector3D();
-			double		getModule() const;
-			Vector3D	copy() const;
-			Vector3D	getReversed() const;
-			void		multiplyByScalar(const double);
-			void		normalize();
-			void		print() const;
+	inline	double			getModule() const;
+	inline	void			print()		const;
+	inline 	const Point3D&	getPoint()	const;
+			Vector3D		copy()		const;
+			void			multiplyByScalar(const double);
+			void			normalize();
 
-			static		Vector3D	add(Vector3D&, Vector3D&);
-			static		Vector3D	substract(Vector3D&, Vector3D&);
-			static		Vector3D	vectorMultiply(Vector3D&, Vector3D&) ;
-			static		double		scalarMultiply(Vector3D&, Vector3D&);
-			static		double		sin(Vector3D&, Vector3D&);
-			static		double		cos(Vector3D&, Vector3D&);
-			static		double		angle(Vector3D&, Vector3D&);
+
+	static 	Vector3D	add(Vector3D&, Vector3D&);
+	static	Vector3D	substract(Vector3D&, Vector3D&);
+	static	Vector3D	vectorMultiply(Vector3D&, Vector3D&) ;
+	static	double		scalarMultiply(Vector3D&, Vector3D&);
+	static	double		sin(Vector3D&, Vector3D&);
+	static	double		cos(Vector3D&, Vector3D&);
+	static	double		angle(Vector3D&, Vector3D&);
 	};
 }
 
