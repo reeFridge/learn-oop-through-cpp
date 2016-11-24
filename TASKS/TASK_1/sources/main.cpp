86ea697555283c1f750d870c4f22bcf3bf821a4f
reeFridge <reefridgerator@gmail.com>
2016-11-24 19:01:27 +0700

    Removed debug code

--- TASKS/TASK_1/sources/main.cpp
+++ TASKS/TASK_1/sources/main.cpp
@@ -7,5 +7,10 @@ int main(int argc, char** argv) {
 		cout << "[DEBUG IS ON]" << endl;
 	#endif // DEBUG
 	
-	Vector3D v();
+	Vector3D v(1.0);
+	Vector3D v1(2.0, 3.0, 1.0);
+	Vector3D v2;
+    v.print();
+	v1.print();
+	v2.print();
 }
